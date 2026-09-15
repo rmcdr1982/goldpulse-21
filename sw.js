5b020cf137fd24cfa6b4de11e0124afc7318901f
rmcdr1982 <rmcdr1982@gmail.com>
2026-09-15 10:37:35 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='goldpulse-v21-2';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.add('./','./index.html','./manifest.webmanifest')).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{let cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
